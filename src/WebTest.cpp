394e4294389a35af7125200665cded236120afc7
Gabriel H Gosselin <you@example.com>
2021-11-05 19:18:35 -0400

    Went back to main script, kept seperate html pages

--- src/WebTest.cpp
+++ src/WebTest.cpp
@@ -1,29 +1,54 @@
 
 //===========================================================================================
-// ------------------------------- load libraries and classes start -------------------------
+// ------------------------------- load libraries and classes start -------------------------------
 //===========================================================================================
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
-#include "WebServer.hpp"
+#include "page1.h"
+#include "page2.h"
 
 const char *ssid = "Beemaster_3000";
 const char *password = "12345678";
+
+ESP8266WebServer server(80);
 
 //===========================================================================================
 // ------------------------------- program subfunctions start -------------------------------
 //===========================================================================================
 
+void handleRoot() 
+{
+  server.send(200, "text/html", htmlPage1);
+}
+
+void handlePage2() 
+{
+  server.send(200, "text/html", htmlPage2);
+}
 
 //===========================================================================================
 // ------------------------------- Initializing Arduino start -------------------------------
 //===========================================================================================
 void setup() 
 {  
-  Serial.begin(115200);
- 
+ Serial.begin(115200);
+  
+  // pageDisplayCounter=0;
   WiFi.softAP(ssid, password);
-  WebServer::setup();
+ 
+  Serial.println();
+  Serial.print("Server IP address: ");
+  Serial.println(WiFi.softAPIP());
+  Serial.print("Server MAC address: ");
+  Serial.println(WiFi.softAPmacAddress());
+ 
+  server.on("/", handleRoot);
+  server.on("/page2", handlePage2);
+  server.begin();
+ 
+  Serial.println("Server listening"); 
 }
 
 //===========================================================================================
@@ -31,5 +56,5 @@ void setup()
 //===========================================================================================
 void loop()
 {
-  WebServer::handleClient();
+ server.handleClient();
 }
