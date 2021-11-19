bb2fa8f97d3a978e38fef948719cd0f56fd9910a
Gabriel H Gosselin <you@example.com>
2021-11-18 21:15:52 -0500

    Webserver Functional, hyperlinks to old data not

--- lib/WebServer/page1.h
+++ lib/WebServer/page1.h
@@ -5,11 +5,17 @@ R"=====(
   </head>  
   <body>    
     <h1>Beemaster 3000 HOME</h1>
-    <p><a href="/data"> Click here to access the latest data file</a></p>
-    <p>%s</p>
+    <p><a href="/%s"> Click here to access the latest data file</a></p>
+    <table>
+      <tr>
+        <td>File Name &emsp;</td>
+        <td>Size (Bytes) &emsp;</td>
+        <td>Date Created &emsp;&emsp;&emsp;</td>
+        <td>Last Modified</td>
+      </tr>
+      %s
+    </table>
     <p>number of page loads: %i</p>
   </body>  
 </html>
 )=====";
-
-
--- src/WebTest.cpp
+++ src/WebTest.cpp
@@ -20,15 +20,16 @@ IPAddress subnet(255,255,255,0);
 ESP8266WebServer server(80);
 
 int short pageDisplayCounter;
+int short filesInCard;
 
 // SD card
 File file;
-File root;
+// File root;
 #define SD_CS_PIN D8 // SD chip select pin.
 #define FILE_BASE_NAME "Data"
 char fileName[11] = "Data04.csv";
-// char fileName[13] = "TESTFILE.txt";
-char fileList[500];
+char fileNameWebServer[15]="/";
+char fileListHTML[1024];
 
 
 
@@ -37,46 +38,55 @@ char fileList[500];
 //===========================================================================================
 
 
-void printDirectory(File dir) 
+void getFileList(File dir) 
 {
-  const char *buffName = entry.name();
+  memset(fileListHTML, '\0', sizeof(fileListHTML));
+  filesInCard=0;
    while(true) 
    {
-     File entry =  dir.openNextFile();
-     if (! entry) 
+     File entry = dir.openNextFile();
+    if (! entry) 
       {
         // no more files
         // return to the first file in the directory
         dir.rewindDirectory();
         break;
       }
-
-      // strcat(fileList,buffName);
-      // insert creation date here using file.getCreateDateTime();
-      // strcat(fileList,"<br>" );
-
-      char buff[32];
+    if(filesInCard==0)
+      {
+        filesInCard++;
+        continue;
+      }
+      // get file information
+      char BuffSize[10];
+      itoa (entry.size(),BuffSize,10);
+      char buffTime[17];
       struct tm ts;
-      time_t epochTime = entry.getCreationTime();
-      ts = *localtime(&epochTime);
-      strftime(buff, sizeof(buff), "%Y-%m-%d %H:%M", &ts);
-      Serial.print(entry.name());
-      Serial.print("\t");
-      Serial.print(entry.size(), DEC);
-      Serial.print("\t");
-      Serial.println(buff);
+      time_t epochTimeCreate = entry.getCreationTime();
+      time_t epochTimeWrite = entry.getLastWrite();
+      // create file list
+      strcat(fileListHTML,"<tr><td><a href=/" );
+      strcat(fileListHTML,entry.name());
+      strcat(fileListHTML,">" );
+      strcat(fileListHTML,entry.name());
+      strcat(fileListHTML,"</a></td><td>");
+      strcat(fileListHTML,BuffSize);
+      strcat(fileListHTML,"</td><td>");
+      ts = *localtime(&epochTimeCreate);
+      strftime(buffTime, sizeof(buffTime), "%Y-%m-%d %H:%M", &ts);
+      strcat(fileListHTML,buffTime);
+      strcat(fileListHTML,"</td><td>");
+      ts = *localtime(&epochTimeWrite);
+      strftime(buffTime, sizeof(buffTime), "%Y-%m-%d %H:%M", &ts);
+      strcat(fileListHTML,buffTime);
+      strcat(fileListHTML,"</td></tr>");
+      // print to terminal
+      // Serial.print(entry.name());
+      // Serial.print("\t");
+      // Serial.print(BuffSize);
+      // Serial.print("\t");
+      // Serial.println(buffTime);
    }
-}
-
-
-void openReadFile()
-{
-  file = SD.open(fileName, FILE_READ);
-}
-
-void openWriteFile()
-{
-  file = SD.open(fileName, FILE_WRITE);
 }
 
 void handleNotFound()
@@ -85,29 +95,40 @@ void handleNotFound()
 }
 void handleRoot() 
 {
-  char charBuffer[1024];
+  
+  File root;
+  root = SD.open("/");
+  getFileList(root);
+  root.close();
+
+  char charBuffer[2048];
   pageDisplayCounter++;
-  sprintf(charBuffer,htmlPage1,fileList,pageDisplayCounter);
+  sprintf(charBuffer,htmlPage1,fileName,fileListHTML,pageDisplayCounter);
   server.send(200, "text/html", charBuffer);
 }
 
 void handleDataPage() 
 {
-  openReadFile();
-  int SDfsize  = file.size();
+  File fileToStream;
+  fileToStream = SD.open(fileName, FILE_READ);
+  int SDfsize  = fileToStream.size();
   server.sendHeader("Content-Length",(String)(SDfsize));
   server.sendHeader("Cache-Control","max-age=2628000,public");
-  size_t fsizeSent = server.streamFile(file,"text/csv");
+  size_t fsizeSent = server.streamFile(fileToStream,"text/csv");
 
   Serial.print("File Size: ");
   Serial.println(SDfsize);
   Serial.print("File Size sent: ");
   Serial.println(fsizeSent);
 
-  file.close();
+  fileToStream.close();
   delay(100);
 }
 
+void createStreamData()
+{
+  handleNotFound();
+}
 
 //===========================================================================================
 // ------------------------------- Initializing Arduino start -------------------------------
@@ -126,9 +147,7 @@ void setup()
   }
   Serial.println("card initialized.");
 
-  root = SD.open("/");
-  printDirectory(root);
-  root.close();
+
 
   //// Web Server
   WiFi.softAPConfig(local_IP, gateway, subnet);
@@ -140,11 +159,14 @@ void setup()
   Serial.print("Server MAC address: ");
   Serial.println(WiFi.softAPmacAddress());
  
+  strcat(fileNameWebServer,fileName);  
+  
   server.on("/", handleRoot);
-  server.on("/data", handleDataPage);
+  server.on(fileNameWebServer, handleDataPage);
+  server.on("/GetData", createStreamData);
   server.onNotFound(handleNotFound);
   server.begin();
- 
+
   Serial.println("Server listening"); 
 
 }
@@ -161,81 +183,19 @@ void loop()
 // ------------------------------- Backup code start ----------------------------------------
 //===========================================================================================
 
+// void openReadFile()
+// {
+//   file = SD.open(fileName, FILE_READ);
+// }
 
-
-  // char fileList[(fileNumber+10)*sizeof(fileName)];
-  // char loop_Buffer[11];
-  // memset(loop_Buffer, '\0', sizeof(loop_Buffer));
-  // memset(fileList, '\0', sizeof(fileList));
-  // for (uint8_t i = 0; i < fileNumber; i++)
-  // {
-  //   strncpy(loop_Buffer, fileName, 11);
-  //   strcat(fileList,loop_Buffer);
-  //   strcat(fileList," <br>" );
-  // }
+// void openWriteFile()
+// {
+//   file = SD.open(fileName, FILE_WRITE);
+// }
 
 
   // fileNumber=atoi(&(fileName[4]));
 
 
 
-  //   //// SD Card 
-  // // Initialize at the highest speed supported by the board that is
-  // // not over 50 MHz. Try a lower speed if SPI errors occur.
-  // pinMode(SD_CS_PIN, OUTPUT);
-  // if (!card.begin(SD_CS_PIN, SD_SCK_MHZ(25))) 
-  // {
-  //   card.initErrorHalt();
-  // }
-  //   if (!root.open("/")) 
-  // {
-  //   card.errorHalt("open root failed");
-  // }
 
-  // memset(fileList, '\0', sizeof(fileList));
-  // char loop_charBuffer[15];
-  // while (file.openNext(&root, O_RDONLY)) 
-  // {
-  //   file.getName(loop_charBuffer, sizeof(loop_charBuffer));
-  //   strcat(fileList,loop_charBuffer);
-  //   // insert creation date here using file.getCreateDateTime();
-  //   strcat(fileList,"<br>" );
-  //   file.close();
-  // }
-
-
-
-//   void handleDataPage() 
-// {
-//   Serial.println(fileName);
-//   if(card.exists(fileName)){
-//     char buf[512];
-//     FsFile fileStream = card.open(fileName, O_RDONLY);
-//     size_t sizeStream = fileStream.size();
-//     server.setContentLength(sizeStream);
-//     server.send(200,"text/html", "");
-//     while(sizeStream) 
-//     {
-//       size_t nread = fileStream.readBytes(buf, sizeof(buf));
-//       server.client().write(buf, nread);
-//       sizeStream -= nread;
-//     }
-//     fileStream.close();
-//   }else
-//   {
-//     server.send(200, "text/html", htmlPage2);
-//   }
-
-//   // server.send(200, "text/html", htmlPage2);
-// }
-
-
-
-  // //// SD Card Initialization (using SD.h Library)
-  // Serial.println();
-  // Serial.print("Initializing SD card...");
-  // if (!SD.begin(SD_CS_PIN)) 
-  // {
-  //   Serial.println("Card failed, or not present");
-  // }
-  // Serial.println("card initialized.");
