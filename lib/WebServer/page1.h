2ca711ab207b2919393ff885d5c6091e78612d9a
Gabriel H Gosselin <you@example.com>
2021-11-19 17:36:16 -0500

    Updated main html Page

--- lib/WebServer/page1.h
+++ lib/WebServer/page1.h
@@ -5,7 +5,7 @@ R"=====(
   </head>  
   <body>    
     <h1>Beemaster 3000 HOME</h1>
-    <p><a href="/%s"> Click here to download the latest data file</a></p>
+    <p><a href="/%s"> Click here to download the latest data file: %s</a></p>
     <table>
       <tr>
         <td>File Name &emsp;</td>
